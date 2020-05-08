acd9cca3b2c4b82b4bf50de573bcb8b77acbe4f9
aintitfun <opolofdez@gmail.com>
2020-05-08 19:02:45 +0200

    more easy to use debug.sh to detec which dbg binary use, 32 or 64

--- debug.sh
+++ debug.sh
@@ -1,2 +1,9 @@
+lscpu | grep Arch | grep 64
+if [ $? -eq 0 ]
+then
 ./dbgp_tcldebug -dbgp localhost:9000 -app-file milogo.tcl \
         -app-shell /usr/bin/wish
+else 
+./dbgp_tcldebug_32 -dbgp localhost:9000 -app-file milogo.tcl \
+        -app-shell /usr/bin/wish
+fi
