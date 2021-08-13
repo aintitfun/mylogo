deac1cc2561bf63e05b71b656ac742781e46616d
aintitfun <opolofdez@gmail.com>
2021-08-13 21:39:37 +0200

    fixed issue with re command, set a little logo file exaple

--- turtle.tcl
+++ turtle.tcl
@@ -10,7 +10,7 @@ array set positionNew {
 
 set isPenDown 1
 
-set logoCommands {av gd gi repite haz bp sl bla fd lt rt cs bk pu pd repeat forward left right clearscreen back penup pendown}
+set logoCommands {av re gd gi repite haz bp sl bla fd lt rt cs bk pu pd repeat forward left right clearscreen back penup pendown}
 set myhistory ""
 
 set pencilColor 1
